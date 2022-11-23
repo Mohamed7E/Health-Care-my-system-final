1331e91503545508ce8707985c02384b86c87efc
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2022-11-23 20:19:33 +0200

    create data

---
+++ Health/function.cs
@@ -0,0 +1,46 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health
+{
+    class function
+    {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private DataTaple dt;
+        private sqlDataAdaper sda;
+        private string Constr;
+
+        public function()
+        {
+            Constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\fg\Documents\HealthCareDB.mdf;Integrated Security=True;Connect Timeout=30";
+            con = new SqlConnection(Constr);
+            cmd = new SqlCommand();
+            cmd.connection = con;
+        }
+        public DataTable GetData(string Query)
+        {
+            dt = new DataTaple();
+            sda = new sqlDataAdaper(Query, con);
+            sda.Fill(dt);
+            return dt;
+        }
+        public int setData (string Query)
+        {
+            int Cnt = 0;
+            if (con.state == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            cmd.CommandText = Query;
+            Cnt = cmd.ExecuteNonQuery();
+            return Cnt;
+        }
+    }
+}
